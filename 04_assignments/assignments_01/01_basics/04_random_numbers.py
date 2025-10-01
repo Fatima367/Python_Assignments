1510789b35bf11608da169817c935fad806f373e
Fatima367 <170196704+Fatima367@users.noreply.github.com>
2025-10-01 20:39:04 +0500

    removed unused code

--- 04_assignments/assignments_01/01_basics/04_random_numbers.py
+++ 04_assignments/assignments_01/01_basics/04_random_numbers.py
@@ -5,7 +5,7 @@ MIN_VALUE: int = 1
 MAX_VALUE: int = 100
 
 def main():
-    for i in range(N_NUMBERS):
+    for _ in range(N_NUMBERS):
         print(random.randint(MIN_VALUE, MAX_VALUE), end=" ")
 
 
